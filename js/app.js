6f1840e6e9ea8110b40ee12af0664085b80a154a
Matthijs Kralt <matthijs29@live.com>
2018-01-18 09:36:46 +0100

    feat: Create list with all classnames for cards

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*
  * Create a list that holds all of your cards
  */
-var cards = document.getElementsByClassName('deck');
+var cards = ["fa-paper-plane-o", "fa-paper-plane-o", "fa-anchor", "fa-anchor", "fa-bolt", "fa-bolt", "fa-cube", "fa-cube", "fa-leaf", "fa-leaf", "fa-diamond", "fa-diamond", "fa-bomb", "fa-bomb", "fa-bicycle", "fa-bicycle"];
 
 /*
  * Display the cards on the page
