a56963cd9035104a3e4b99c1c9afa036d9d2a43e
Matthijs Kralt <matthijs29@live.com>
2018-01-20 11:08:48 +0100

    feat: Enlarge icons, change match background color, last changes to README and text layout
    This is the first version that is submitted.

--- js/app.js
+++ js/app.js
@@ -24,7 +24,8 @@ function generateBoard() {
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length,
+    temporaryValue, randomIndex;
 
   while (currentIndex !== 0) {
     randomIndex = Math.floor(Math.random() * currentIndex);
